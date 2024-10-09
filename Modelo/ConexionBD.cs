086c9ee5670c04e1d967507c71bbfec029a4041c
BlackHawk30-prog <luigihr2001@gmail.com>
2024-10-08 19:52:34 -0600

    Agregar archivos de proyecto.

---
+++ Modelo/ConexionBD.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Modelo
+{
+    public class ConexionBD
+    {
+        private string cadena = "server=localhost; database=pasantias; user=root; password=";
+        public MySqlConnection conectar;
+        public void AbrirConexion()
+        {
+
+            try
+            {
+                conectar = new MySqlConnection();
+                conectar.ConnectionString = cadena;
+                conectar.Open();
+                System.Diagnostics.Debug.WriteLine("Conexion Exitosa");
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                // Console.WriteLine(ex.StackTrace);
+
+            }
+
+        }
+        public void CerarConexion()
+        {
+
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+
+        }
+    }
+}
