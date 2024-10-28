25692abdfec73714b7f8a1dc0a44c588560d9c69
MAguilar-stack <kisamasteve2016@gmail.com>
2024-10-28 10:58:22 -0700

    Pull cambios actualizados

--- Pasantias/RH.aspx.cs
+++ Pasantias/RH.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,17 @@ namespace Pasantias
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
+        RH rH;
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                rH = new RH();
+                rH.grid_rh(GridView2);
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
