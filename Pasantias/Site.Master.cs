cdf076dc23941f86574d7705915cb9495f0bd897
BlackHawk30-prog <luigihr2001@gmail.com>
2024-11-04 13:34:44 -0600

    El login ya casi funcional

--- Pasantias/Site.Master.cs
+++ Pasantias/Site.Master.cs
@@ -1,17 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
 
 namespace Pasantias
 {
-    public partial class SiteMaster : MasterPage
+    public partial class SiteMaster : System.Web.UI.MasterPage
     {
+        // Declaración de los controles HTML Anchor
+        protected HtmlAnchor HojaTiempoNav;
+        protected HtmlAnchor FinanzasNav;
+        protected HtmlAnchor OficialNav;
+        protected HtmlAnchor PostulacionesNav;
+        protected HtmlAnchor RHNav;
+        protected HtmlAnchor SeguridadNav;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Establecer los href con ResolveClientUrl
+            HojaTiempoNav.HRef = ResolveClientUrl("~/Hoja_Tiempo.aspx");
+            FinanzasNav.HRef = ResolveClientUrl("~/Finanzas.aspx");
+            OficialNav.HRef = ResolveClientUrl("~/Oficial.aspx");
+            PostulacionesNav.HRef = ResolveClientUrl("~/Postulaciones.aspx");
+            RHNav.HRef = ResolveClientUrl("~/RH.aspx");
+            SeguridadNav.HRef = ResolveClientUrl("~/Seguridad.aspx");
 
+            // Oculta todos los módulos por defecto
+            HojaTiempoNav.Visible = false;
+            FinanzasNav.Visible = false;
+            OficialNav.Visible = false;
+            PostulacionesNav.Visible = false;
+            RHNav.Visible = false;
+            SeguridadNav.Visible = false;
+
+            // Depuración temporal para verificar el rol en sesión
+            if (Session["UserRol"] == null)
+            {
+                Response.Write("No hay rol en la sesión.");
+            }
+            else
+            {
+                Response.Write("Rol en sesión: " + Session["UserRol"]);
+            }
+
+            // Verifica si el rol está en la sesión
+            if (Session["UserRol"] != null)
+            {
+                int rol = int.Parse(Session["UserRol"].ToString());
+
+                // Controla la visibilidad de cada módulo según el rol
+                if (rol == 1)
+                {
+                    HojaTiempoNav.Visible = true;
+                    PostulacionesNav.Visible = true;
+                }
+                else if (rol == 2)
+                {
+                    HojaTiempoNav.Visible = true;
+                    FinanzasNav.Visible = true;
+                }
+                else if (rol == 5)
+                {
+                    OficialNav.Visible = true;
+                }
+                else if (rol == 6)
+                {
+                    RHNav.Visible = true;
+                }
+                else if (rol == 9)
+                {
+                    SeguridadNav.Visible = true;
+                }
+            }
         }
     }
 }
