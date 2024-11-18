048930a2b631f0f9123df495ada9083ead6f61e8
BlackHawk30-prog <luigihr2001@gmail.com>
2024-11-18 09:27:31 -0600

    Camino de postulaciones casi al 100

--- Pasantias/Postulaciones_Seguridad.aspx.cs
+++ Pasantias/Postulaciones_Seguridad.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using Modelo;
 using System;
+using System.Net.Mail;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -19,7 +20,7 @@ namespace Pasantias
 
             if (!IsPostBack)
             {
-                string condicionSeguridad = "u.RHConfirmado = 1 AND SConfirmado = 0 ";
+                string condicionSeguridad = "u.RHConfirmado = 1 AND SConfirmado = 0";
                 Postulacion.grid_aplicantes(grid_aplicantes, condicionSeguridad);
             }
         }
@@ -31,16 +32,28 @@ namespace Pasantias
                 int idUsuario = Convert.ToInt32(e.CommandArgument);
                 // Llamar al método para aceptar la postulación
                 Postulacion.AceptarPostulacionSeguridad(idUsuario);
+
                 // Volver a cargar la tabla
-                Postulacion.grid_aplicantes(grid_aplicantes);
+                string condicionSeguridad = "u.RHConfirmado = 1 AND SConfirmado = 0";
+                Postulacion.grid_aplicantes(grid_aplicantes, condicionSeguridad);
             }
             else if (e.CommandName == "Rechazar")
             {
                 int idUsuario = Convert.ToInt32(e.CommandArgument);
                 // Llamar al método para rechazar la postulación
                 Postulacion.RechazarPostulacion(idUsuario);
+
+                // Obtener el correo del usuario por DNI y enviar el correo de rechazo
+                string dni = Postulacion.ObtenerDNIporIDUsuario(idUsuario); // Método para obtener el DNI basado en el IDUsuario
+                string correo = Postulacion.ObtenerCorreoPorDNI(dni);
+                if (!string.IsNullOrEmpty(correo))
+                {
+                    EnviarCorreoRechazo(correo);
+                }
+
                 // Volver a cargar la tabla
-                Postulacion.grid_aplicantes(grid_aplicantes);
+                string condicionSeguridad = "u.RHConfirmado = 1 AND SConfirmado = 0";
+                Postulacion.grid_aplicantes(grid_aplicantes, condicionSeguridad);
             }
             else if (e.CommandName == "Detalles")
             {
@@ -49,5 +62,36 @@ namespace Pasantias
                 Response.Redirect($"Detalle_Postulante.aspx?IDUsuario={idUsuario}");
             }
         }
+
+        private void EnviarCorreoRechazo(string destinatario)
+        {
+            try
+            {
+                using (MailMessage mensaje = new MailMessage())
+                {
+                    mensaje.From = new MailAddress("hreyesfotos@gmail.com");
+                    mensaje.To.Add(destinatario);
+                    mensaje.Subject = "Notificación de rechazo de pasantía";
+                    mensaje.Body = "Estimado(a) postulante,\n\nLamentamos informarle que su solicitud para nuestra pasantía no ha sido aceptada. Le agradecemos por su interés y le deseamos éxito en sus futuros proyectos.\n\nSaludos cordiales,\nEl equipo de Pasantías";
+                    mensaje.IsBodyHtml = false;
+
+                    using (SmtpClient clienteSmtp = new SmtpClient())
+                    {
+                        clienteSmtp.Host = "smtp.gmail.com";
+                        clienteSmtp.Port = 587;
+                        clienteSmtp.Credentials = new System.Net.NetworkCredential("hreyesfotos@gmail.com", "ovqx ypvm vtbt fttp");
+                        clienteSmtp.EnableSsl = true;
+
+                        clienteSmtp.Send(mensaje);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores en el envío del correo
+                Console.WriteLine($"Error al enviar el correo de rechazo: {ex.Message}");
+            }
+        }
     }
 }
+
