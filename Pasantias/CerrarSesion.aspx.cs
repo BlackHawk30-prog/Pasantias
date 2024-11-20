2572fd9b9abfd5f32d77826384c68141c54d0546
BlackHawk30-prog <luigihr2001@gmail.com>
2024-11-20 14:28:33 -0600

    Cerrar sesion aplicado

---
+++ Pasantias/CerrarSesion.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Pasantias
+{
+    public partial class CerrarSesion : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Verificar si el usuario está autenticado
+            if (Session["UserID"] != null)
+            {
+                // Limpiar todas las variables de sesión
+                Session.Clear();
+
+                // Abandonar la sesión actual
+                Session.Abandon();
+
+                // Redirigir al usuario a la página de inicio de sesión
+                Response.Redirect("~/login.aspx", false);
+            }
+            else
+            {
+                // Si no hay sesión activa, redirigir directamente al inicio de sesión
+                Response.Redirect("~/login.aspx", false);
+            }
+        }
+    }
+}
